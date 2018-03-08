0205ec1ea79278e985595cc6961a9b9a69968219
Pierre Martin <pierre@occitech.fr>
2018-03-09 00:09:53 +0100

    Super simple service worker

---
+++ serviceworker.js
@@ -0,0 +1,40 @@
+// Licensed under a CC0 1.0 Universal (CC0 1.0) Public Domain Dedication
+// http://creativecommons.org/publicdomain/zero/1.0/
+// @see https://adactio.com/journal/13540
+// @source https://gist.github.com/adactio/3717b7da007a9363ddf21f584aae34af
+
+// HTML files: try the network first, then the cache.
+// Other files: try the cache first, then the network.
+// Both: cache a fresh version if possible.
+// (beware: the cache will grow and grow; there's no cleanup)
+
+const cacheName = "files";
+
+addEventListener("fetch", fetchEvent => {
+  const request = fetchEvent.request;
+  if (request.method !== "GET") {
+    return;
+  }
+  fetchEvent.respondWith(
+    (async function() {
+      const responseFromFetch = fetch(request);
+      fetchEvent.waitUntil(
+        (async function() {
+          const responseCopy = (await responseFromFetch).clone();
+          const myCache = await caches.open(cacheName);
+          await myCache.put(request, responseCopy);
+        })()
+      );
+      if (request.headers.get("Accept").includes("text/html")) {
+        try {
+          return await responseFromFetch;
+        } catch (error) {
+          return caches.match(request);
+        }
+      } else {
+        const responseFromCache = await caches.match(request);
+        return responseFromCache || responseFromFetch;
+      }
+    })()
+  );
+});
